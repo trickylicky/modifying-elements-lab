a9a6ead4f057481c10371b034c1fb490d1241817
Tricky Licky <wangombevictor5@gmail.com>
2022-08-14 17:49:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = ("victory");
+
+newHeader.textContent = "YOUR-NAME is the champion"; 
